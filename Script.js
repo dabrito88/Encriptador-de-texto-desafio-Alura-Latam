a5065297fbbb5052ae3d7816f3dca7cdbcee4d23
dabrito88 <dabrito.dw24@gmail.com>
2024-03-17 21:45:33 -0300

    primer envio

---
+++ Script.js
@@ -0,0 +1,16 @@
+const campo_texto = document.querySelector("#inputText")
+const campo_encriptado = document.querySelector("#exputText")
+
+const matriz_code = [
+    ["e","enter"], // indice 0
+    ["i","imes"], // indice 1 
+    ["a","ai"], // indice 2
+    ["o","ober"], // indice 3
+    ["u","ufat"], // indice 4
+
+]
+
+function btnEncriptar(){
+    const texto = campo_texto.value; 
+    console.log(campo_texto)
+}
